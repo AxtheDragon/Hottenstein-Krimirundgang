5eed06d6dc0912bdd884c9b024d9dbe9f7cf2143
Azure7111 <azure.diamond7111@googlemail.com>
2016-11-20 19:30:45 +0100

    Add Tour and Tour Collection classes

---
+++ Krimirundgang/Tour.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Krimirundgang
+{
+    public class Tour
+    {
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        public Tour(string name, string content = null)
+        {
+            this.Name = name;
+            this.Content = content;
+        }
+    }
+}
