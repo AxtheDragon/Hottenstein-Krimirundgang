a199d9e935679e91ad365e3dd285d4257e3a60a7
Andi <ax.the.dragon@googlemail.com>
2016-12-06 20:52:02 +0100

    Added Stops in TourContent.cs

--- Krimirundgang/TourContent.cs
+++ Krimirundgang/TourContent.cs
@@ -29,6 +29,9 @@ namespace Krimirundgang
         //Information Text of the Stop
         public string mText;
 
+        //Preview Text
+        public string mPreview;
+
         //return Stop ID
         public int StopID
         {
@@ -68,7 +71,43 @@ namespace Krimirundgang
                       mTitle = "Erster Halt",
                       mLat = 52.0001000,
                       mLong = 40.300000,
-                      mText = System.Resources.ResourceManager.GetString(Resource.String.stop01)
+                      mPreview = "01 Vorschautext für die Liste",
+                      mText = "01 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            },
+            new Stop {mStopID = 2,
+                      mTitle = "Zweiter Halt",
+                      mLat = 52.1001000,
+                      mLong = 40.400000,
+                      mPreview = "02 Vorschautext für die Liste",
+                      mText = "02 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            },
+            new Stop {mStopID = 3,
+                      mTitle = "Dritter Halt",
+                      mLat = 52.0201000,
+                      mLong = 40.600000,
+                      mPreview = "03 Vorschautext für die Liste",
+                      mText = "03 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            },
+            new Stop {mStopID = 4,
+                      mTitle = "Vierter Stop",
+                      mLat = 52.3201000,
+                      mLong = 41.000000,
+                      mPreview = "04 Vorschautext für die Liste - Hier steht mehr!",
+                      mText = "04 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            },
+            new Stop {mStopID = 5,
+                      mTitle = "Fünfter Halt",
+                      mLat = 52.8201000,
+                      mLong = 41.000000,
+                      mPreview = "05 Vorschautext für die Liste",
+                      mText = "05 Lorem ipsum dolor sit amet, Hier steht mal was daziwschen consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            },
+            new Stop {mStopID = 6,
+                      mTitle = "Sechster Stop",
+                      mLat = 51.0201000,
+                      mLong = 39.800000,
+                      mPreview = "06 Vorschautext für die Liste",
+                      mText = "06 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
             }
 
         };
