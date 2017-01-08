dcd6d6b4e6b1d08a0148d50bb843d23626242a46
Andi <ax.the.dragon@googlemail.com>
2017-01-08 17:29:12 +0100

    Loading Stop Layout when loading StopActivity

--- Krimirundgang/StopActivity.cs
+++ Krimirundgang/StopActivity.cs
@@ -18,8 +18,11 @@ namespace Krimirundgang
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Stop);
 
-            // Create your application here
+
         }
     }
 }
