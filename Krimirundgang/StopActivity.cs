89aeded54fa619fbf618779e4ec5e769ecd56dc2
Andi <ax.the.dragon@googlemail.com>
2017-01-08 17:55:20 +0100

    removed some empty lines

--- Krimirundgang/StopActivity.cs
+++ Krimirundgang/StopActivity.cs
@@ -22,20 +22,14 @@ namespace Krimirundgang
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Stop);
 
-            int stopID = Intent.GetIntExtra("StopID", 1);
-            Stop stop = new Tour().Stops.First(s => s.StopID == stopID);
-
             //Get references for Layout elements
             TextView title = FindViewById<TextView>(Resource.Id.textViewTitle);
             TextView longitude = FindViewById<TextView>(Resource.Id.textViewLong);
             TextView latitude = FindViewById<TextView>(Resource.Id.textViewLat);
             TextView text = FindViewById<TextView>(Resource.Id.textViewText);
 
-            title.Text = stop.Title;
-            longitude.Text = stop.Long.ToString();
-            latitude.Text = stop.Lat.ToString();
-            text.Text = stop.Text;
+            title.Text = "Blub";
 
-        }
+         }
     }
 }
